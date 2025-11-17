7d8cc73130249599750174a97c6d81538f3dc9d8
Alan <al4nch3n@gmail.com>
2025-11-16 19:13:40 -0500

    Oak Donuts OD update GUI and setup

--- src/App.java
+++ src/App.java
@@ -1,7 +1,10 @@
 import gui.MainFrame;
+import database.DatabaseHelper;
 
 public class App {
     public static void main(String[] args) {
+        DatabaseHelper.initializeDatabase();
+        System.out.println("Database OK");
         javax.swing.SwingUtilities.invokeLater(() -> {
             MainFrame frame = new MainFrame();
             frame.setVisible(true);
